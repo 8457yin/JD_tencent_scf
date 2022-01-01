37f56f78c1fe60efd9610d392b58eafbaacfaef9
8457yin <91551555+8457yin@users.noreply.github.com>
2022-01-01 22:34:54 +0800

    Update jdPetShareCodes.js

--- jdPetShareCodes.js
+++ jdPetShareCodes.js
@@ -8,25 +8,7 @@
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PetShareCodes = [
   //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTAxODc2NTEzMDAwMDAwMDAxMzgwNTcyNw==@MTAxODc2NTEzMzAwMDAwMDAxMzgwNDg3OQ==@MTE1NDAxNzcwMDAwMDAwMzUxNDMwMDc=@MTE1NDQ5MzYwMDAwMDAwMzUxNDMwMTE=@MTE1NDUwMTI0MDAwMDAwMDM2OTQ2Mjk1',
+  'MTEyNjE4NjQ2MDAwMDAwMDU5NDYyNjMz@MTEyNzEzMjc0MDAwMDAwMDUzNjQwOTI1',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTAxODc2NTEzMDAwMDAwMDAxMzgwNTcyNw==@MTAxODc2NTEzMzAwMDAwMDAxMzgwNDg3OQ==@MTE1NDAxNzcwMDAwMDAwMzUxNDMwMDc=@MTE1NDQ5MzYwMDAwMDAwMzUxNDMwMTE=@MTE1NDUwMTI0MDAwMDAwMDM2OTQ2Mjk1',
+  'MTEyNjE4NjQ2MDAwMDAwMDU5NDYyNjMz@MTEyNzEzMjc0MDAwMDAwMDUzNjQwOTI1',
 ]
-// 判断github action里面是否有东东萌宠互助码
-if (process.env.PETSHARECODES) {
-  if (process.env.PETSHARECODES.indexOf('&') > -1) {
-    console.log(`您的东东萌宠互助码选择的是用&隔开\n`)
-    PetShareCodes = process.env.PETSHARECODES.split('&');
-  } else if (process.env.PETSHARECODES.indexOf('\n') > -1) {
-    console.log(`您的东东萌宠互助码选择的是用换行隔开\n`)
-    PetShareCodes = process.env.PETSHARECODES.split('\n');
-  } else {
-    PetShareCodes = process.env.PETSHARECODES.split();
-  }
-} else if (process.env.JD_COOKIE) {
-  console.log(`您secret里面未提供助力码，优先进行自己账号内互助，然后再给脚本内置的码进行助力，请知晓！`)
-}
-for (let i = 0; i < PetShareCodes.length; i++) {
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['PetShareCode' + index] = PetShareCodes[i];
-}
